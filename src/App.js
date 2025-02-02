82a30cf989fb34d79ac841d3686c4171685990be
Maeve Crossan <maevecrossan@gmail.com>
2025-02-02 16:07:24 +0000

    Use react-bootstrap to build basic, responsive navbar with favicon and logo.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Button from "react-bootstrap/Button";
+import NavBar from "./components/NavBar";
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello, World!</h1>
-        </div>
+
+			<NavBar/>
+
+		</div>
     );
 }
 
