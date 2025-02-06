37a758d9b87f04ce8f60ed95882fc4c3a5bca368
Maeve Crossan <maevecrossan@gmail.com>
2025-02-06 18:52:17 +0000

    Create 'create post' page (BUG: cannot post yet).

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,7 +27,7 @@ const NavBar = () => {
         <NavLink 
             className={styles.NavLink} 
             activeClassName={styles.Active} 
-            to="/post/create"
+            to="/posts/create"
             >
                 <i className="fas fa-plus-square"></i>Add Post
         </NavLink>
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -9,72 +9,70 @@ export const SetCurrentUserContext = createContext();
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
-export const CurrentUserProvider = ({children}) => {
+export const CurrentUserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const history = useHistory();
 
     const handleMount = async () => {
         try {
-            const {data} = await axiosRes.get('dj-rest-auth/user/');
-            setCurrentUser(data);
+        const { data } = await axiosRes.get("dj-rest-auth/user/");
+        setCurrentUser(data);
         } catch (err) {
-            console.log(err);
+        console.log(err);
         }
-    }
+    };
 
     useEffect(() => {
         handleMount();
-    }, [])
+    }, []);
 
     useMemo(() => {
         axiosReq.interceptors.request.use(
-            async (config) => {
-                try {
-                    await axios.post('/dj-rest-auth/token/refresh');
-                } catch(err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push('/signin')
-                        }
-                        return null
-                    });
-                    return config
+        async (config) => {
+            try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+            } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+                if (prevCurrentUser) {
+                history.push("/signin");
                 }
-                return config
-            },
-            (err) => {
-                return Promise.reject(err);
+                return null;
+            });
+            return config;
             }
-        )
+            return config;
+        },
+        (err) => {
+            return Promise.reject(err);
+        }
+        );
 
         axiosRes.interceptors.response.use(
-            (response) => response,
-            async (err) => {
-                if (err.response?.status === 401) {
-                    try {
-                    await axios.post("/dj-rest-auth/token/refresh/");
-                    } catch (err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                        history.push("/signin");
-                        }
-                        return null;
-                    });
-                    }
-                    return axios(err.config);
+        (response) => response,
+        async (err) => {
+            if (err.response?.status === 401) {
+            try {
+                await axios.post("/dj-rest-auth/token/refresh/");
+            } catch (err) {
+                setCurrentUser((prevCurrentUser) => {
+                if (prevCurrentUser) {
+                    history.push("/signin");
                 }
-                return Promise.reject(err);
-                }
-            );
-        }, [history]);
-
-        
+                return null;
+                });
+            }
+            return axios(err.config);
+            }
+            return Promise.reject(err);
+        }
+        );
+    }, [history]);
 
     return (
         <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <SetCurrentUserContext.Provider value={setCurrentUser}>
             {children}
-            </SetCurrentUserContext.Provider>
+        </SetCurrentUserContext.Provider>
         </CurrentUserContext.Provider>
-    )
-}
+    );
+};
